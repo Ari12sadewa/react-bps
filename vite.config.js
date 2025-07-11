2c93f8d13f4212e9658251b19d0c779f62b28b5d
Ari12sadewa <ngurah.arisadewa@gmail.com>
2025-07-12 03:23:05 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-react(),tailwindcss(),
-],
+    react(), tailwindcss(),
+  ], build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
