f165b1b17e0a9fc94d69556f76a13fbd9f3d6f0b
Ari12sadewa <43287768+Ari12sadewa@users.noreply.github.com>
2025-07-12 19:04:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
-  plugins: [react()],
-  build: {
+  plugins: [
+    react(), tailwindcss(),
+  ], build: {
     outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
